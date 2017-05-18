0c890c9ba87002312350e0edeaa99a7edf3e14ee
bloodpn <ferdi.pn@idn-solution.com>
2017-05-18 17:21:14 +0700

    sore 5-18-2017

--- app/Http/Controllers/MasterjadwalController.php
+++ app/Http/Controllers/MasterjadwalController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 use App\Http\Controllers\Controller;
+use App\Models\Jadwal;
 
 class MasterjadwalController extends Controller
 {
     public function index()
     {
+    	$jadwals = Jadwal::all();
+    	dd($jadwals);
+
     	return view('master/masterjadwal');
     }
 }
